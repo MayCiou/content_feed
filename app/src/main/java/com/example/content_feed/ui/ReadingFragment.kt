d026ab7f0fd7a96109e3cb19bf39c589c5253e0c
May <a8349696@gmail.com>
2026-09-20 08:12:51 +0800

    add 2 fragment

---
+++ app/src/main/java/com/example/content_feed/ui/ReadingFragment.kt
@@ -0,0 +1,12 @@
+package com.example.content_feed.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.content_feed.R
+
+class ReadingFragment : Fragment(R.layout.fragment_reading) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
